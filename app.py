23ff673af46d4a7d64a34852f746ea3b0fce989b
Anand-S23 <anandsroopsing23@gmail.com>
2022-04-26 15:32:37 -0400

    Running intune prep tool command + error handling

--- app.py
+++ app.py
@@ -8,8 +8,25 @@ paths = {
     "output": ""
 }
 
-def submit():
-    pass
+def reset(setup, source, output, result):
+    paths["setup"] = ""
+    paths["source"] = ""
+    paths["ouput"] = ""
+
+    setup.config(text="Select Setup Folder")
+    source.config(text="Select Source File")
+    output.config(text="Select Ouput Folder")
+    result.config(text="Success!")
+
+def submit(setup, source, output, result):
+    # Check if there is issue with paths
+    if paths["setup"] == "" or paths["source"] == "" or paths["output"] == "":
+        result.config(text="One of the inputs are missing, fill to continue")
+        return
+
+    #os.system(f"IntuneWinAppUtil -c {paths['setup']} -s {paths['source']} -o {paths['output']}")
+    os.system(f"echo {paths['setup']}")
+    reset(setup, source, output, result)
 
 def select_file(key, widget, is_directory=True):
     if is_directory:
@@ -21,7 +38,6 @@ def select_file(key, widget, is_directory=True):
         paths[key] = filename
         new_label = f"'{filename}'"
         widget.config(text=new_label)
-        print(paths[key])
 
 def init_app():
     window = tk.Tk()
@@ -32,7 +48,7 @@ def init_app():
 def main():
     window = init_app()
     app_font = ('Arial', 18, 'bold')
-    path_font = ('Arial', 14)
+    result_font = ('Arial', 14)
     title_width = 300
 
     # Title
@@ -40,21 +56,25 @@ def main():
     title = tk.Label(window, text='Intune Prep Tool GUI', font=app_font)  
     title.place(x=title_x, y=0)
 
+    # Result
+    result = tk.Label(window, text='Hello world', font=result_font)  
+    result.place(x=title_x, y=300)
+
     # Setup Row
-    setup = tk.Button(window, text='Select Setup Folder', width=67, command=lambda:select_file("setup", setup))
-    setup.place(x=5, y=50)
+    setup = tk.Button(window, text='Select Setup Folder', width=66, command=lambda:select_file("setup", setup))
+    setup.place(x=8, y=50)
 
     # Source Row
     source = tk.Button(window, text='Select Source File', width=66, command=lambda:select_file("source", source, False))
-    source.place(x=5, y=100)
+    source.place(x=8, y=100)
 
     # Output Row
-    output = tk.Button(window, text='Select Ouput Folder', width=65, command=lambda:select_file("output", output))
-    output.place(x=5, y=150)
+    output = tk.Button(window, text='Select Ouput Folder', width=66, command=lambda:select_file("output", output))
+    output.place(x=8, y=150)
 
     # Submit Button
-    output = tk.Button(window, text='Submit', width=64, command=lambda:submit())
-    output.place(x=5, y=200)
+    submit_btn = tk.Button(window, text='Submit', width=66, command=lambda:submit(setup, source, output, result))
+    submit_btn.place(x=8, y=200)
 
     # Update App
     window.mainloop()
